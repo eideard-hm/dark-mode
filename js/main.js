04f0f9901806feb260ccb2ca21ebdeedd1f67735
Edier Heraldo Hernández Molano <edierhernandezmo@gmail.com>
2021-04-22 21:09:10 -0500

    Agregue los archivos de dark-mode

---
+++ js/main.js
@@ -0,0 +1,24 @@
+btnSwitch = document.getElementById('switch');
+
+btnSwitch.addEventListener('click', () => {
+    document.body.classList.toggle('dark');
+    btnSwitch.classList.toggle('active');
+
+    //guardamos el modo actual en el que estamos
+    //classList.contains: permite saber si la clase contiene
+    if (document.body.classList.contains('dark')) {
+        localStorage.setItem('dark-mode', 'true');
+    } else {
+        localStorage.setItem('dark-mode', 'false');
+    }
+});
+
+//obtener el modo actual en el que estamos
+
+if (localStorage.getItem('dark-mode') === 'true') {
+    document.body.classList.add('dark');
+    btnSwitch.classList.add('active');
+} else {
+    document.body.classList.remove('dark');
+    btnSwitch.classList.remove('active');
+}
